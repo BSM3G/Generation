c22f5a4eb063fab4063552f240073c2a1efba092
Dylan Teague <dylan.o.teague@vanderbilt.edu>
2016-05-30 15:54:35 -0500

    fixing it up

--- Slurm/Analyze_Grid/defaults/tntAnalyze_default.sh
+++ Slurm/Analyze_Grid/defaults/tntAnalyze_default.sh
@@ -2,23 +2,28 @@
 
 date
 
-cd WORK_AREA
+cd /gpfs21/scratch/teaguedo/workspace/test/CMSSW_7_4_15/src/Analyze_Grid
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export slc6_amd64_gcc491
 eval `scramv1 runtime -sh`
 
-run_num=$[ $1 + 1 ]
+run_num=$[ $1 ]
 input_sample=$2
 
 infilename=$(tail -n+${run_num} list_Samples/${input_sample}.txt | head -n1)
 outfilename=${input_sample}.${run_num}.root
 
 infilename=${infilename/\\/}
+infilename=${infilename/cmseos/cmsxrootd}
+infilename=${infilename/\/ra2tau/}
 
-cd $CMSSW_BASE/src/Analyzer/BSM3G_TNT_MainAnalyzer/ANALYSISDIRECTORY
+echo $infilename
+echo $outfilename
+
+cd $CMSSW_BASE/src/Analyzer/BSM3G_TNT_MainAnalyzer/VBFEWKinoAnalysis_diMuonChannel_SR
 ./BSM3GAnalyzer $infilename $outfilename
 
-cp $outfilename ${CMSSW_BASE}/src/Files/Analysis/WORK_DIRECTORY/${input_sample}
+cp $outfilename ${CMSSW_BASE}/src/Files/Analysis/test/${input_sample}
 
 
 
